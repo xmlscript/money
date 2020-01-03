c1d65c674f0e8b0ccdeefe7ef620be24b7df0020
xmlscript <xmlscript@gmail.com>
2020-01-03 21:51:00 +0800

    init

---
+++ birthday.php
@@ -0,0 +1,12 @@
+<?php namespace money; // vim: se fdm=marker:
+
+/**
+ * @todo 距离明年此时（生日倒计时）
+ */
+class birthday extends day{
+
+  function __construct(string $str){
+
+  }
+
+}
---
+++ time.php
@@ -0,0 +1,16 @@
+<?php namespace money; // vim: se fdm=marker:
+
+/**
+ * @todo 时段
+ */
+class time{
+
+  function __construct(string $str){
+
+  }
+
+  function ago():string{
+    return '';
+  }
+
+}
